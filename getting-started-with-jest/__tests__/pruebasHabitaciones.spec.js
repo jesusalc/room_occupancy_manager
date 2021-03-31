163811ac92e92a2f5b1805310ed0f68892f1f11b
AV-MMI <villalobos.aaron@hotmail.com>
2021-03-31 18:29:23 -0400

    adding the test to check the rooms and their earnings

--- getting-started-with-jest/__tests__/pruebasHabitaciones.spec.js
+++ getting-started-with-jest/__tests__/pruebasHabitaciones.spec.js
@@ -1,4 +1,11 @@
-const clientes = [
+/* ---TO KEEP IN MIND---
+
+I replace the "toBe" to "toStrictEqual" because it didnt let me proceed with my
+array comparation. For more information: https://stackoverflow.com/questions/56839801/jest-js-error-received-serializes-to-the-same-string
+
+*/
+
+const clients = [
   23,
   45,
   155,
@@ -12,21 +19,77 @@ const clientes = [
 ]
 
 function checarHabitaciones(Premium, Economy) {
-  clientes.map()
+  let aboveHundred = clients.filter(n => n >= 100).sort((a, b) => b - a);
+  let belowHundred = clients.filter( n => n < 100).sort((a, b) => b - a);
+
+  let premiumProfits = 0;
+  //loop where we take the first premium number of clients and what each one pay for his/her room and then we sums all this numbers to obtain the total earning for our Premium room's.
+  if(Premium <= aboveHundred.length){
+    for(let i = 0; i < Premium; i++){
+      premiumProfits += aboveHundred[i];
+    }
+  }
+
+  //If all of ours economy rooms are full, then send that clients to a Premium Room.
+  if(Premium > aboveHundred.length){
+    const hostUpgrade = Math.abs(aboveHundred.length - Premium)
+
+    for(let i = 0; i < hostUpgrade; i++){
+      aboveHundred.push(belowHundred[i]);
+    };
+
+    belowHundred = belowHundred.slice(hostUpgrade);
+
+    for(let i = 0; i < Premium; i++){
+      premiumProfits += aboveHundred[i];
+    };
+  }
+
+  let economyProfits = 0;
+  //loop where we take the first economy number of clients and what each one pay for his/her room and then we sums all this numbers to obtain the total earning for our economy room's.
+  if(Economy <= belowHundred.length){
+    for(let i = 0; i < Economy; i++){
+      economyProfits += belowHundred[i];
+    }
+  }
+
+
+  let totalEarnings = [premiumProfits, economyProfits]
+  return totalEarnings;
 }
+
+let Premium;
+let Economy;
+
 describe("Prueba 1", () => {
   test("Habitaciones gratuitas Premium: 3 Habitaciones gratuitas Economy: 3",() => {
-    const Premium = 3;
-    const Economy = 3;
-    expect(checarHabitaciones(Premium, Economy)).toBe([738, 167]);
+    Premium = 3;
+    Economy = 3;
+    expect(checarHabitaciones(Premium, Economy)).toStrictEqual([738, 167]);
   });
 });
+
+
 describe("Prueba 2", () => {
-  test("Habitaciones Premium gratuitas: 7 Habitaciones Economy gratuitas: 5",() => {});
+  test("Habitaciones Premium gratuitas: 6 Habitaciones Economy gratuitas: 4",() => {
+    Premium = 6;
+    Economy = 4;
+    expect(checarHabitaciones(Premium, Economy)).toStrictEqual([1054, 189]);
+  });
 });
+
 describe("Prueba 3", () => {
-  test("Habitaciones Premium gratuitas: 2 Habitaciones Economy gratuitas: 7",() => {});
+  test("Habitaciones Premium gratuitas: 2 Habitaciones Economy gratuitas: 4",() => {
+    Premium = 2;
+    Economy = 4;
+    expect(checarHabitaciones(Premium, Economy)).toStrictEqual([583, 189]);
+  });
 });
+
 describe("Prueba 4", () => {
-  test("Habitaciones Premium gratuitas: 7 Habitaciones Economy gratuitas: 1",() => {});
+  test("Habitaciones Premium gratuitas: 7 Habitaciones Economy gratuitas: 1",() => {
+    Premium = 7;
+    Economy = 1;
+    expect(checarHabitaciones(Premium, Economy)).toStrictEqual([1153, 45]);
+  });
 });
